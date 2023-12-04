39de153fbf680daa3472b238add5b4d9ee8ec638
Problem7451 <112329096+Problem7451@users.noreply.github.com>
2023-12-04 18:01:38 -0500

    modif Panier en singleton + Recyleview Panier

--- app/src/main/java/com/example/pfijava/ListeAchat.java
+++ app/src/main/java/com/example/pfijava/ListeAchat.java
@@ -1,18 +1,37 @@
 package com.example.pfijava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class ListeAchat extends AppCompatActivity {
+    private Panier panier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_achat);
+
+        ArrayList<Article> arrArticle = new ArrayList<Article>();
+        RecyclerView recycleAchat = (RecyclerView) findViewById(R.id.achat_RecyclerView);
+
+        PanierAdapter adaptateur = new PanierAdapter(getApplicationContext());
+        recycleAchat.setAdapter(adaptateur);
+
+        recycleAchat.setLayoutManager(new LinearLayoutManager(this));
+
+        recycleAchat.setItemAnimator(new DefaultItemAnimator());
+        recycleAchat.setHasFixedSize(true);
+
 
         Button btnVersListe = (Button) findViewById(R.id.achat_btnVersListe);
 
@@ -23,6 +42,5 @@ public class ListeAchat extends AppCompatActivity {
                 startActivity(versLE);
             }
         });
-
     }
 }
