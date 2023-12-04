6b31b1559a5bb7c7881118eb8bc99369f6283a6d
Problem7451 <112329096+Problem7451@users.noreply.github.com>
2023-12-04 12:54:55 -0500

    Panier liste d'achat

--- app/src/main/java/com/example/pfijava/ListeAchat.java
+++ app/src/main/java/com/example/pfijava/ListeAchat.java
@@ -2,7 +2,10 @@ package com.example.pfijava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListeAchat extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ListeAchat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_achat);
+
+        Button btnVersListe = (Button) findViewById(R.id.achat_btnVersListe);
+
+        btnVersListe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent versLE = new Intent(ListeAchat.this, ListeEpicerie.class);
+                startActivity(versLE);
+            }
+        });
+
     }
 }
