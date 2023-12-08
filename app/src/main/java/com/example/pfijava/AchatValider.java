c60fab1e796109e049ae1a56d9edfd20bde582ee
Problem7451 <112329096+Problem7451@users.noreply.github.com>
2023-12-08 17:09:06 -0500

    Commande - video

--- app/src/main/java/com/example/pfijava/AchatValider.java
+++ app/src/main/java/com/example/pfijava/AchatValider.java
@@ -3,26 +3,60 @@ package com.example.pfijava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 public class AchatValider extends AppCompatActivity {
     private Panier panier;
+    VideoView videoView;
+    boolean executeThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achat_valider);
-        panier = Panier.getInstance();
+//        panier = Panier.getInstance();
 
         Button btnRetour = (Button) findViewById(R.id.btn_retour);
         btnRetour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent versLE = new Intent(AchatValider.this, ListeEpicerie.class);
+                executeThread = false;
                 startActivity(versLE);
             }
         });
+
+        videoView = (VideoView) findViewById(R.id.AV_videoView_valider);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.valider);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                executeThread = true;
+                while (executeThread) {
+                    videoView.start();
+                }
+            }
+        }).start();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        executeThread = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        executeThread = false;
+    }
+
 }
